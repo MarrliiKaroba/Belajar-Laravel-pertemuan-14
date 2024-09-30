8196049cbed785f60a9dbb0dab1096e5559163ce
marli karoba <marrliikaroba02@gmail.com>
2024-09-30 20:11:39 +0900

    Create data.php

---
+++ data.php
@@ -0,0 +1,38 @@
+<?php 
+
+
+$mahasiswa = [
+
+    [
+        'id' => 1,
+        'nim' => "2020118827",
+        'nama' => "Marli Karoba",
+        'alamat' => "Sorong",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 2,
+        'nim' => "14123577",
+        'nama' => "Jhon",
+        'alamat' => "Sorong",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 3,
+        'nim' => "16411021",
+        'nama' => "Juliana",
+        'alamat' => "Sorong",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 4,
+        'nim' => "16411021",
+        'nama' => "Matius",
+        'alamat' => "Sorong",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+];
